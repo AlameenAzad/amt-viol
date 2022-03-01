17fa22da7aa6d3c11cd4f298b3086a61b2680896
Alameen Azad <alameen.azad@protonmail.com>
2022-03-01 15:23:04 +0300

    change route settings

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,20 @@
 const routes = [
   {
     path: "/",
-    name: "home",
 
-        meta: { requiresAuth: true },
     component: () => import("layouts/MainLayout.vue"),
     children: [
       {
         path: "",
-        component: () => import("pages/Index.vue"),
-        meta: { requiresAuth: true },
-      },
-    ],
+        name: "landing",
+        component: () => import("pages/Index.vue")
+      }
+    ]
   },
   {
     path: "/:catchAll(.*)*",
-    component: () => import("pages/Error404.vue"),
-  },
+    component: () => import("pages/Error404.vue")
+  }
 ];
 
 export default routes;
