ac80c552fe0c603ecd9fc8abb6e76610d8ee2784
bilendm <bilendmohammed@gmail.com>
2022-03-04 15:13:51 +0300

    Card Section (In Progess)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,46 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <q-card class="my-card">
+            <q-card-section>
+              <div class="row">
+                <div class="col-12 text-center">
+                  <q-img
+                    src="https://placeimg.com/500/300/nature"
+                    style="height: 60px; max-width: 60px"
+                  />
+                </div>
+                <div class="col-12 text-center">
+                  <h3 class="font-16 text-weight-bold q-mb-none">
+                    Project Ideas
+                  </h3>
+                  <p class="q-mb-none">
+                    Short Description fusce vulputate eleifend sapien.
+                  </p>
+                </div>
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-4">
+          <q-card class="my-card">
+            <q-card-section>
+              Card Two
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="col-4">
+          <q-card class="my-card">
+            <q-card-section>
+              Card Three
+            </q-card-section>
+          </q-card>
+        </div>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -11,3 +52,11 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+/* TODO change card class name */
+.my-card {
+  width: 100%;
+  max-width: 370px;
+}
+</style>
