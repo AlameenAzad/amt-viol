e432523ba6c41a5018c256b94e3f0e8629f10249
bilendm <bilendmohammed@gmail.com>
2022-03-04 20:56:42 +0300

    Created Document View page

---
+++ src/pages/DocumentView.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h1 class="font-36 text-weight-600">Document View</h1>
+        </div>
+        <div class="col-12">
+          Document View
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "documentView",
+  data() {
+    return {};
+  },
+  mounted() {
+    console.log("Documents mounted");
+    console.log(this.$route.query);
+  }
+};
+</script>
+
+<style></style>
