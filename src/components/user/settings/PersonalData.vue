6644311832df8c23520a1d192f044a58ddf1797a
bilendm <bilendmohammed@gmail.com>
2022-03-08 01:30:18 +0300

    Created Component for each settings tab

---
+++ src/components/user/settings/PersonalData.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <h6 class="text-center font-24 q-mt-md">Personal Data</h6>
+    <q-form
+      ref="personalDataForm"
+      @submit.prevent="savePersonalData"
+      class="q-gutter-lg q-px-md q-mb-md"
+    >
+      <div class="row items-center">
+        <div class="col-3">
+          <p class="font-16 no-margin">
+            Email
+          </p>
+        </div>
+        <div class="col-9">
+          <q-input
+            outlined
+            class="no-shadow input-radius-6"
+            v-model="email"
+            :rules="[]"
+          />
+        </div>
+      </div>
+      <div class="row items-center">
+        <div class="col-3">
+          <p class="font-16 no-margin">
+            Current Password
+          </p>
+        </div>
+        <div class="col-9">
+          <q-input
+            outlined
+            class="no-shadow input-radius-6"
+            v-model="currentPassword"
+            :rules="[]"
+          />
+        </div>
+      </div>
+      <div class="row items-center">
+        <div class="col-3">
+          <p class="font-16 no-margin">
+            New Password
+          </p>
+        </div>
+        <div class="col-9">
+          <q-input
+            outlined
+            class="no-shadow input-radius-6"
+            v-model="newPassword"
+            :rules="[]"
+          />
+        </div>
+      </div>
+      <div class="row items-center">
+        <div class="col-3">
+          <p class="font-16 no-margin">
+            Repeat New Password
+          </p>
+        </div>
+        <div class="col-9">
+          <q-input
+            outlined
+            class="no-shadow input-radius-6"
+            v-model="newPasswordConfirm"
+            :rules="[]"
+          />
+        </div>
+      </div>
+      <q-separator inset class="bg-blue opacity-10" />
+      <div class="row items-center">
+        <div class="col-3">
+          <p class="font-16 no-margin">
+            Transfer Data
+          </p>
+        </div>
+        <div class="col-9">
+          <q-btn
+            outline
+            no-caps
+            size="16px"
+            color="primary"
+            class="text-weight-600 q-py-xs"
+            label="Select User"
+          />
+        </div>
+      </div>
+      <q-separator inset class="bg-blue opacity-10" />
+      <div class="row items-center">
+        <div class="col-12">
+          <q-btn
+            flat
+            no-caps
+            icon="delete"
+            size="16px"
+            color="red"
+            class="text-red q-py-xs"
+            label="Delete General Data"
+          >
+          </q-btn>
+        </div>
+      </div>
+      <q-separator inset class="bg-blue opacity-10" />
+      <div class="row justify-center">
+        <q-btn
+          label="Save Changes"
+          type="submit"
+          size="16px"
+          color="primary"
+          no-caps
+          class="radius-6 q-px-xl q-py-sm"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "personalDataTab",
+  data() {
+    return {
+      email: "",
+      currentPassword: "",
+      newPassword: "",
+      newPasswordConfirm: ""
+    };
+  },
+  methods: {
+    savePersonalData() {
+      this.$refs.personalDataForm.validate().then(success => {
+        if (success) {
+          console.log("success");
+        } else {
+          console.log("error");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/pages/dashboard/Settings.vue
+++ src/pages/dashboard/Settings.vue
@@ -27,236 +27,14 @@
         <div class="col-7">
           <q-tab-panels class="radius-20 shadow-1" v-model="tab" animated>
             <q-tab-panel name="generalData">
-              <h6 class="text-center font-24 q-mt-md">General Data</h6>
-              <q-form
-                ref="generalDataForm"
-                @submit.prevent="saveGeneralData"
-                class="q-gutter-lg q-px-md q-mb-md"
-              >
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Email
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="email"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Current Password
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="currentPassword"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      New Password
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="newPassword"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Repeat New Password
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="newPasswordConfirm"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <q-separator inset class="bg-blue opacity-10" />
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Transfer Data
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-btn
-                      outline
-                      no-caps
-                      size="16px"
-                      color="primary"
-                      class="text-weight-600 q-py-xs"
-                      label="Select User"
-                    />
-                  </div>
-                </div>
-                <q-separator inset class="bg-blue opacity-10" />
-                <div class="row items-center">
-                  <div class="col-12">
-                    <q-btn
-                      flat
-                      no-caps
-                      icon="delete"
-                      size="16px"
-                      color="red"
-                      class="text-red q-py-xs"
-                      label="Delete General Data"
-                    >
-                    </q-btn>
-                  </div>
-                </div>
-                <q-separator inset class="bg-blue opacity-10" />
-                <div class="row justify-center">
-                  <q-btn
-                    label="Save Changes"
-                    type="submit"
-                    size="16px"
-                    color="primary"
-                    no-caps
-                    class="radius-6 q-px-xl q-py-sm"
-                  />
-                </div>
-              </q-form>
+              <GeneralData />
             </q-tab-panel>
-
             <q-tab-panel name="personalData">
-              <h6 class="text-center font-24 q-mt-md">Personal Data</h6>
-              <q-form
-                ref="generalDataForm"
-                @submit.prevent="saveGeneralData"
-                class="q-gutter-lg q-px-md q-mb-md"
-              >
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Email
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="email"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Current Password
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="currentPassword"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      New Password
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="newPassword"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Repeat New Password
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-input
-                      outlined
-                      class="no-shadow input-radius-6"
-                      v-model="newPasswordConfirm"
-                      :rules="[]"
-                    />
-                  </div>
-                </div>
-                <q-separator inset class="bg-blue opacity-10" />
-                <div class="row items-center">
-                  <div class="col-3">
-                    <p class="font-16 no-margin">
-                      Transfer Data
-                    </p>
-                  </div>
-                  <div class="col-9">
-                    <q-btn
-                      outline
-                      no-caps
-                      size="16px"
-                      color="primary"
-                      class="text-weight-600 q-py-xs"
-                      label="Select User"
-                    />
-                  </div>
-                </div>
-                <q-separator inset class="bg-blue opacity-10" />
-                <div class="row items-center">
-                  <div class="col-12">
-                    <q-btn
-                      flat
-                      no-caps
-                      icon="delete"
-                      size="16px"
-                      color="red"
-                      class="text-red q-py-xs"
-                      label="Delete General Data"
-                    >
-                    </q-btn>
-                  </div>
-                </div>
-                <q-separator inset class="bg-blue opacity-10" />
-                <div class="row justify-center">
-                  <q-btn
-                    label="Save Changes"
-                    type="submit"
-                    size="16px"
-                    color="primary"
-                    no-caps
-                    class="radius-6 q-px-xl q-py-sm"
-                  />
-                </div>
-              </q-form>
+              <PersonalData />
             </q-tab-panel>
 
             <q-tab-panel name="notifications">
-              <div class="text-h6">Movies</div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              <Notifications />
             </q-tab-panel>
           </q-tab-panels>
         </div>
@@ -266,27 +44,20 @@
 </template>
 
 <script>
+import GeneralData from "components/user/settings/GeneralData.vue";
+import PersonalData from "components/user/settings/PersonalData.vue";
+import Notifications from "components/user/settings/Notifications.vue";
 export default {
   name: "settings",
+  components: {
+    GeneralData,
+    PersonalData,
+    Notifications
+  },
   data() {
     return {
-      tab: "personalData",
-      email: "",
-      currentPassword: "",
-      newPassword: "",
-      newPasswordConfirm: ""
+      tab: "personalData"
     };
-  },
-  methods: {
-    saveGeneralData() {
-      this.$refs.generalDataForm.validate().then(success => {
-        if (success) {
-          console.log("success");
-        } else {
-          console.log("error");
-        }
-      });
-    }
   }
 };
 </script>
