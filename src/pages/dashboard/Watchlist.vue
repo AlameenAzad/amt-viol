9065ff3715be14af3f4aee590d06afb7c5d73167
AlameenAzad <alameen.azad@protonmail.com>
2022-03-11 19:53:48 +0300

    watchlist page

---
+++ src/pages/dashboard/Watchlist.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="q-mx-xl q-mt-lg">
+    <overview />
+  </q-page>
+</template>
+
+<script>
+import overview from "components/watchlist/overview.vue";
+export default {
+  name: "watchlist",
+  components: {
+    overview
+  },
+  data() {
+    return {};
+  }
+};
+</script>
