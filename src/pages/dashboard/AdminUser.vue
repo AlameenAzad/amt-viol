05b05e902d693ccb4de9063eaf3f025b815988d3
Ravyar19 <ravyar.ara@gmail.com>
2022-03-16 00:02:04 +0300

    AdminUser Final

--- src/pages/dashboard/AdminUser.vue
+++ src/pages/dashboard/AdminUser.vue
@@ -19,8 +19,112 @@
             </q-input>
           </div>
           <div class="col-auto  ">
-            <q-btn color="primary" label="Add User" icon="person_add" />
+            <q-btn
+              color="primary"
+              label="Add User"
+              icon="person_add"
+              @click="showDialog = true"
+            />
           </div>
+          <q-dialog v-model="showDialog">
+            <q-card class="q-pa-lg">
+              <div>
+                <h6 class="text-center font-24 q-mt-md">Invite User</h6>
+                <q-form
+                  ref="personalDataForm"
+                  class="q-gutter-lg q-px-md q-mb-md "
+                >
+                  <div class="row items-center ">
+                    <div class="col-3">
+                      <p class="font-14 no-margin">
+                        Email*
+                      </p>
+                    </div>
+                    <div class="col-9">
+                      <q-input
+                        outlined
+                        class="no-shadow input-radius-6"
+                        v-model="email"
+                        :rules="[]"
+                        label="E-Mail"
+                      />
+                    </div>
+                  </div>
+                  <div class="row items-center ">
+                    <div class="col-3 ">
+                      <p class="font-14 no-margin ">
+                        Role*
+                      </p>
+                    </div>
+                    <div class="col-9">
+                      <q-select
+                        outlined
+                        :options="options"
+                        class="no-shadow input-radius-6"
+                        v-model="role"
+                        :rules="[]"
+                        label="Editor"
+                      />
+                    </div>
+                  </div>
+                  <div class="row items-center">
+                    <div class="col-3">
+                      <p class="font-14 no-margin">
+                        Administration
+                      </p>
+                    </div>
+                    <div class="col-9">
+                      <q-select
+                        outlined
+                        :options="options"
+                        class="no-shadow input-radius-6"
+                        v-model="administration"
+                        :rules="[]"
+                        label="Select"
+                      />
+                    </div>
+                  </div>
+                  <div class="row items-center justify-evenly">
+                    <div class="col-3">
+                      <p class="font-14 no-margin">
+                        Message
+                      </p>
+                    </div>
+                    <div class="col-9">
+                      <q-input
+                        outlined
+                        class="no-shadow input-radius-6"
+                        v-model="message"
+                        :rules="[]"
+                        type="textarea"
+                        label="text"
+                      />
+                    </div>
+                  </div>
+
+                  <div class="row justify-center q-ml-lg ">
+                    <q-btn
+                      label="Cancel"
+                      outline
+                      type="submit"
+                      size="16px"
+                      color="primary"
+                      no-caps
+                      class="radius-6 q-px-xl q-py-sm q-mr-sm "
+                    />
+                    <q-btn
+                      label="Send Invitation"
+                      type="submit"
+                      size="16px"
+                      color="primary"
+                      no-caps
+                      class="radius-6 q-px-xl q-py-sm "
+                    />
+                  </div>
+                </q-form>
+              </div>
+            </q-card>
+          </q-dialog>
         </div>
       </template>
       <template v-slot:header="props">
@@ -75,11 +179,19 @@
     </q-table>
   </div>
 </template>
+
 <script>
 export default {
   name: "dataOverview",
   data() {
     return {
+      options: ["Google", "Facebook", "Twitter", "Apple", "Oracle"],
+      showDialog: false,
+      role: "",
+      filter: "",
+      administration: "",
+      email: "",
+      message: "",
       data: [
         {
           name: "Frozen Yogurt",
