e3730268eb843f059ebd346d71a419135b03af4c
bilendm <bilendmohammed@gmail.com>
2022-03-17 20:26:57 +0300

    Changed /stats page component

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,7 @@
-const routes = [
-  {
+const routes = [{
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [
-      {
+    children: [{
         path: "",
         name: "landing",
         component: () => import("pages/Index.vue")
@@ -23,8 +21,7 @@ const routes = [
   {
     path: "/dashboard",
     component: () => import("layouts/dashboardLayout.vue"),
-    children: [
-      {
+    children: [{
         path: "/dashboard",
         name: "Home",
         meta: {
@@ -106,22 +103,21 @@ const routes = [
         component: () => import("pages/dashboard/Home.vue")
       },
       {
-        path: "/Stats",
+        path: "/stats",
         name: "Statistics&Archive",
         meta: {
           title: "Statistics & Archive",
           showInNavigation: true
         },
         icon: "/icons/Archive.svg",
-        component: () => import("pages/dashboard/Home.vue")
+        component: () => import("pages/dashboard/Stats.vue")
       }
     ]
   },
   {
     path: "/user",
     component: () => import("layouts/dashboardLayout.vue"),
-    children: [
-      {
+    children: [{
         path: "/user/notifications",
         name: "notifications",
         meta: {
