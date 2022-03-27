5ee46ca2e6706e6c70efdc4e9341df522b76a9a3
bilendm <bilendmohammed@gmail.com>
2022-03-27 16:48:19 +0300

    Added Categories component

---
+++ src/components/projects/create/Categories.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-select
+    outlined
+    dense
+    v-model="model"
+    multiple
+    :options="options"
+    options-selected-class="text-primary"
+    class="no-shadow input-radius-6"
+    @input="onSelect"
+  >
+    <template v-slot:selected>
+      <template v-if="model">
+        <span v-for="(category, index) in model" :key="index">
+          {{ index > 0 ? ", " : "" }}
+          {{ category }}
+        </span>
+      </template>
+      <template v-else>
+        <span class="text-grey">
+          Select Categories
+        </span>
+      </template>
+    </template></q-select
+  >
+</template>
+
+<script>
+export default {
+  name: "categories",
+  data() {
+    return {
+      model: null,
+      options: ["Google", "Facebook", "Twitter", "Apple", "Oracle"]
+    };
+  },
+  methods: {
+    onSelect(value) {
+      this.$emit("update:category", value);
+    }
+  }
+};
+</script>
+
+<style></style>
