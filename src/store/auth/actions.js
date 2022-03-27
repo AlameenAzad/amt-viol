4e6a586452d583af5c21ae3d460d9a01bba90995
bilendm <bilendmohammed@gmail.com>
2022-03-27 22:43:48 +0300

    Added login action

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,4 +1,26 @@
-/*
-export function someAction (context) {
+import { api } from "boot/axios";
+import { Notify } from "quasar";
+
+export function login(context, payload) {
+  const { identifier } = payload;
+  const { password } = payload;
+  api.defaults.headers.common["Authorization"] = null;
+  api
+    .post("/api/auth/local", {
+      identifier,
+      password
+    })
+    .then(response => {
+      console.log("response :>> ", response);
+      context.commit("setUser", response.data);
+      this.$router.push({ path: "/dashboard" });
+    })
+    .catch(error => {
+      console.log("error :>> ", error);
+      Notify.create({
+        position: "top-right",
+        type: "negative",
+        message: error.response.data.error.message
+      });
+    });
 }
-*/
