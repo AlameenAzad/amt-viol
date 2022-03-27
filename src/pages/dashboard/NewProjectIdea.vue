8c4636d2d2e0fb48c3ac06b151d42762709bae1c
bilendm <bilendmohammed@gmail.com>
2022-03-27 16:46:43 +0300

    Additions to new Project Idea form

--- src/pages/dashboard/NewProjectIdea.vue
+++ src/pages/dashboard/NewProjectIdea.vue
@@ -3,10 +3,10 @@
     <p class="text-center font-36 text-weight-regular q-my-lg">
       Project Ideas
     </p>
-    <div class="bg-white radius-20 q-py-lg">
+    <div class="bg-white radius-20 q-py-lg q-px-md">
       <q-form
-        ref="personalDataForm"
-        @submit.prevent="savePersonalData"
+        ref="newProjectIdeaForm"
+        @submit.prevent="submitNewProjectIdea"
         class="q-gutter-lg q-px-md q-mb-md"
       >
         <div class="row items-center">
@@ -137,7 +137,6 @@
             >
               <div class="col-11">
                 <UserSelect
-                  :key="componentKey"
                   :editor="editor"
                   @update:editor="updateEditor(index, $event)"
                 />
@@ -157,7 +156,7 @@
               <q-btn
                 @click="addEditor"
                 outline
-                class="q-mt-md radius-6"
+                class="radius-6"
                 icon="add"
                 size="md"
                 color="primary"
@@ -166,7 +165,7 @@
             </div>
           </div>
         </div>
-        <div class="row items-center">
+        <div class="row items-baseline">
           <div class="col-4">
             <p class="font-16 no-margin">Visibility</p>
           </div>
@@ -191,6 +190,7 @@
               </template></q-select
             >
             <p class="font-16 q-mb-none q-mt-md text-grey">
+              <!-- TODO make this text dynamic -->
               This Document is visible for all Users
             </p>
           </div>
@@ -200,17 +200,298 @@
             <q-separator class="bg-blue opacity-10" />
           </div>
         </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">Filter Categories</p>
+          </div>
+          <div class="col-8">
+            <Categories @update:category="form.categories = $event" />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">Tags*</p>
+          </div>
+          <div class="col-8">
+            <Tags @update:tag="form.tags = $event" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <q-separator class="bg-blue opacity-10" />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              Project content*
+            </p>
+          </div>
+          <div class="col-8">
+            <q-input
+              outlined
+              dense
+              class="no-shadow input-radius-6"
+              placeholder="Describe the project"
+              v-model="form.projectContent"
+              :rules="[]"
+            />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              Project goals*
+            </p>
+          </div>
+          <div class="col-8">
+            <q-input
+              outlined
+              dense
+              class="no-shadow input-radius-6"
+              placeholder="Project goals"
+              v-model="form.projectGoals"
+              :rules="[]"
+            />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              project values &amp; benefits*
+            </p>
+          </div>
+          <div class="col-8">
+            <q-input
+              outlined
+              dense
+              class="no-shadow input-radius-6"
+              placeholder="project value &amp; benefits"
+              v-model="form.projectValuesAndBenefits"
+              :rules="[]"
+            />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              cooperation partners (optional)
+            </p>
+          </div>
+          <div class="col-8">
+            <q-input
+              outlined
+              dense
+              class="no-shadow input-radius-6"
+              placeholder="cooperation partners (optional)"
+              v-model="form.coorperationPartners"
+              :rules="[]"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <q-separator class="bg-blue opacity-10" />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              Investive/ non-investive*
+            </p>
+          </div>
+          <div class="col-8">
+            <q-btn-toggle
+              v-model="form.investiveNoninvestive"
+              spread
+              no-caps
+              toggle-color="yellow"
+              padding="12px 10px"
+              color="transparent"
+              toggle-text-color="black"
+              text-color="black"
+              class="no-shadow toggleGap"
+              :options="investiveNoninvestiveOptions"
+            />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              project status (optional)
+            </p>
+          </div>
+          <div class="col-8">
+            <q-btn-toggle
+              v-model="form.projectStatus"
+              spread
+              no-caps
+              toggle-color="yellow"
+              padding="12px 10px"
+              color="transparent"
+              toggle-text-color="black"
+              text-color="black"
+              class="no-shadow toggleGap"
+              :options="projectStatuses"
+            />
+          </div>
+        </div>
+        <div
+          class="row"
+          :class="
+            form.estimatedCosts.length > 0 ? 'items-baseline' : 'items-center'
+          "
+        >
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              Estimated Costs (optional)
+            </p>
+          </div>
+          <div class="col-8">
+            <EstimatedCost @update:cost="updateEstimatedCosts" />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <q-separator class="bg-blue opacity-10" />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              funding guidlines (optional)
+            </p>
+          </div>
+          <div class="col-8">
+            <q-input
+              outlined
+              dense
+              class="no-shadow input-radius-6"
+              placeholder="Select funding guidlines"
+              v-model="form.fundingGuidelines"
+              :rules="[]"
+            />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <q-separator class="bg-blue opacity-10" />
+          </div>
+        </div>
+        <div class="row items-center">
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              Planned Period
+            </p>
+          </div>
+          <div class="col-8">
+            <div class="row q-col-gutter-x-md">
+              <div class="col-6">
+                <q-input
+                  outlined
+                  dense
+                  class="no-shadow input-radius-6"
+                  v-model="form.startDate"
+                  color="primary"
+                  bg-color="white"
+                  placeholder="Start"
+                >
+                  <template v-slot:append>
+                    <q-icon name="event" class="cursor-pointer">
+                      <q-popup-proxy
+                        ref="qDateProxy"
+                        transition-show="scale"
+                        transition-hide="scale"
+                      >
+                        <q-date v-model="form.startDate" mask="DD.MM.YYYY">
+                          <div class="row items-center justify-end">
+                            <q-btn
+                              v-close-popup
+                              label="Close"
+                              color="primary"
+                              flat
+                            />
+                          </div>
+                        </q-date>
+                      </q-popup-proxy>
+                    </q-icon>
+                  </template>
+                </q-input>
+              </div>
+              <div class="col-6">
+                <q-input
+                  outlined
+                  dense
+                  class="no-shadow input-radius-6"
+                  v-model="form.endDate"
+                  color="primary"
+                  bg-color="white"
+                  placeholder="End"
+                >
+                  <template v-slot:append>
+                    <q-icon name="event" class="cursor-pointer">
+                      <q-popup-proxy
+                        ref="qDateProxy"
+                        transition-show="scale"
+                        transition-hide="scale"
+                      >
+                        <q-date v-model="form.endDate" mask="DD.MM.YYYY">
+                          <div class="row items-center justify-end">
+                            <q-btn
+                              v-close-popup
+                              label="Close"
+                              color="primary"
+                              flat
+                            />
+                          </div>
+                        </q-date>
+                      </q-popup-proxy>
+                    </q-icon>
+                  </template>
+                </q-input>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <q-separator class="bg-blue opacity-10" />
+          </div>
+        </div>
+        <div
+          class="row"
+          :class="
+            form.estimatedCosts.length > 0 ? 'items-baseline' : 'items-center'
+          "
+        >
+          <div class="col-4">
+            <p class="font-16 no-margin">
+              Links
+            </p>
+          </div>
+          <div class="col-8">
+            <Links @update:link="updateEstimatedCosts" />
+          </div>
+        </div>
       </q-form>
     </div>
   </q-page>
 </template>
 
 <script>
-import UserSelect from "components/user/Select/UserSelect.vue";
+import UserSelect from "components/user/select/UserSelect.vue";
+import Categories from "components/projects/create/Categories.vue";
+import Tags from "components/projects/create/Tags.vue";
+import EstimatedCost from "src/components/projects/create/EstimatedCost.vue";
+import Links from "components/projects/create/Links.vue";
 export default {
-  name: "index",
+  name: "newProjectIdea",
   components: {
-    UserSelect
+    UserSelect,
+    Categories,
+    Tags,
+    EstimatedCost,
+    Links
   },
   data() {
     return {
@@ -225,9 +506,31 @@ export default {
         email: "",
         locWithGeoInfo: "",
         editorsInvited: [],
-        visibility: ""
+        visibility: "",
+        categories: [],
+        tags: [],
+        projectContent: "",
+        projectGoals: "",
+        projectValuesAndBenefits: "",
+        coorperationPartners: "",
+        investiveNoninvestive: "",
+        projectStatus: "",
+        estimatedCosts: [],
+        fundingGuidelines: "",
+        startDate: "",
+        endDate: ""
       },
-      visibilityOptions: ["Google", "Facebook"]
+      visibilityOptions: ["Google", "Facebook"],
+      investiveNoninvestiveOptions: [
+        { label: "Investive", value: "investive" },
+        { label: "Nicht-investiv", value: "nonInvestive" }
+      ],
+      projectStatuses: [
+        { label: "Idea", value: "idea" },
+        { label: "Development", value: "development" },
+        { label: "Pre-Planing", value: "pre-planning" },
+        { label: "Detailed-Planning", value: "detailed-planning" }
+      ]
     };
   },
   methods: {
@@ -238,14 +541,26 @@ export default {
       this.form.editorsInvited.push("");
     },
     removeEditor(index) {
-      console.log("index :>> ", index);
       this.form.editorsInvited.splice(index, 1);
       this.forceRerender();
     },
     updateEditor(index, value) {
       this.form.editorsInvited[index] = value;
       this.forceRerender();
+    },
+    updateEstimatedCosts(val) {
+      this.form.estimatedCosts = val;
     }
   }
 };
 </script>
+
+<style lang="scss">
+.toggleGap {
+  gap: 15px;
+  > button {
+    border-radius: 10px !important;
+    border: 1px solid $yellow;
+  }
+}
+</style>
