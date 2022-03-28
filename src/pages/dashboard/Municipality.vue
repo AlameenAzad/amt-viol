c526361bdf00378327a76f1f68280051514864a3
Ravyar19 <ravyar.ara@gmail.com>
2022-03-29 01:50:38 +0300

    MunicipalityPage&MunicipalityDetailPage

---
+++ src/pages/dashboard/Municipality.vue
@@ -0,0 +1,14 @@
+<template>
+  <municipality />
+</template>
+
+<script>
+import municipality from "components/Municipality/municipality.vue";
+
+export default {
+  name: "MunicipalityPage",
+  components: {
+    municipality
+  }
+};
+</script>
