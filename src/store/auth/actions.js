9c7d883e1768b19b35189082539f324fe614fd72
bilendm <bilendmohammed@gmail.com>
2022-04-04 21:28:11 +0300

    Make login function async

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -1,25 +1,22 @@
 import { api } from "boot/axios";
 import { Notify } from "quasar";
 
-export function login(context, payload) {
+export async function login(context, payload) {
   const { identifier } = payload;
   const { password } = payload;
-  api
-    .post("/api/auth/local", {
-      identifier,
-      password
-    })
-    .then(response => {
-      console.log("response :>> ", response);
-      context.commit("setUser", response.data);
+  if (!!identifier && !!password) {
+    try {
+      const res = await api.post("/api/auth/local", { identifier, password });
+      context.commit("setUser", res.data);
       this.$router.push({ path: "/dashboard" });
-    })
-    .catch(error => {
+    } catch (error) {
       console.log("error :>> ", error);
       Notify.create({
         position: "top-right",
         type: "negative",
         message: error.response.data.error.message
       });
-    });
+      return false;
+    }
+  }
 }
