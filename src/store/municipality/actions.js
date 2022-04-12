ea8d5cfac0e5f372080745986d87a3c8c40b7d1e
bilendm <bilendmohammed@gmail.com>
2022-04-12 16:52:50 +0300

    Added edit and delete Municipality

--- src/store/municipality/actions.js
+++ src/store/municipality/actions.js
@@ -30,6 +30,34 @@ export async function createMunicipality(context, payload) {
         message: "Municiplaity added successfully",
         type: "positive"
       });
+      context.dispatch("getMunicipalities");
+    } catch (error) {
+      Notify.create({
+        position: "top-right",
+        type: "negative",
+        message: error.response.data.error.message
+      });
+      return false;
+    }
+  }
+}
+
+export async function editMunicipality(context, payload) {
+  const { id } = payload;
+  const { title } = payload;
+  const { location } = payload;
+  if (!!id && !!title && !!location) {
+    try {
+      const res = await api.put(`/api/municipalities/${id}`, {
+        data: { title, location, updatedAt: new Date().toISOString() }
+      });
+      console.log("res :>> ", res);
+      context.commit("editMunicipality", res.data.data);
+      Notify.create({
+        message: "Municipality updated successfully",
+        type: "positive"
+      });
+      context.dispatch("getMunicipalities");
     } catch (error) {
       Notify.create({
         position: "top-right",
@@ -43,24 +71,22 @@ export async function createMunicipality(context, payload) {
 
 export async function delteMunicipality(context, payload) {
   const { id } = payload;
-  console.log("id :>> ", id);
-  //  if (!!title && !!location) {
-  //    try {
-  //      const res = await api.post("/api/municipalities", {
-  //        data: { title, location }
-  //      });
-  //      context.commit("addMunicipality", res.data.data);
-  //      Notify.create({
-  //        message: "Municiplaity added successfully",
-  //        type: "positive"
-  //      });
-  //    } catch (error) {
-  //      Notify.create({
-  //        position: "top-right",
-  //        type: "negative",
-  //        message: error.response.data.error.message
-  //      });
-  //      return false;
-  //    }
-  //  }
+  if (!!id) {
+    try {
+      const res = await api.delete(`/api/municipalities/${id}`);
+      context.commit("deleteMunicipality", res.data.data && res.data.data.id);
+      Notify.create({
+        message: "Municipality deleted successfully",
+        type: "positive"
+      });
+      context.dispatch("getMunicipalities");
+    } catch (error) {
+      Notify.create({
+        position: "top-right",
+        type: "negative",
+        message: error.response.data.error.message
+      });
+      return false;
+    }
+  }
 }
