ecc92c26b1d546d75165eac10e978ec7fb0ba24f
bilendm <bilendmohammed@gmail.com>
2022-04-14 04:02:00 +0300

    Put LoginDialog into separate component

---
+++ src/components/user/authentication/LoginDialog.vue
@@ -0,0 +1,108 @@
+<template>
+  <q-dialog v-model="$_options">
+    <q-card
+      class="radius-10 q-px-lg q-pt-lg q-pb-md"
+      style="width: 570px; max-width: 80vw;"
+    >
+      <q-card-section>
+        <q-form ref="loginForm" @submit.prevent="login" class="q-gutter-lg">
+          <div class="row">
+            <div class="col-12">
+              <p class="q-mb-sm font-16">Email</p>
+              <q-input
+                outlined
+                dense
+                class="no-shadow input-radius-6"
+                v-model="form.identifier"
+                :rules="[val => !!val || 'Field is required']"
+              />
+            </div>
+            <div class="col-12 q-mt-lg">
+              <p class="q-mb-sm font-16">Password</p>
+              <q-input
+                outlined
+                dense
+                type="password"
+                class="no-shadow input-radius-6"
+                v-model="form.password"
+                :rules="[val => !!val || 'Field is required']"
+              />
+            </div>
+          </div>
+          <p class="no-margin text-red text-center">{{ errorMsg }}</p>
+          <div class="row justify-center">
+            <div class="col-5 text-center">
+              <q-btn
+                type="submit"
+                class="radius-10 full-width"
+                padding="12px 12px"
+                color="yellow"
+                text-color="black"
+                unelevated
+                :loading="isLoading"
+              >
+                Login<template v-slot:loading>
+                  {{ loginMessages }}
+                  <q-circular-progress
+                    indeterminate
+                    class="on-right"
+                  /> </template
+              ></q-btn>
+            </div>
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: "loginDialog",
+  props: {
+    dialogState: { type: Boolean, default: false }
+  },
+  data() {
+    return {
+      form: {
+        identifier: "",
+        password: ""
+      },
+      errorMsg: "",
+      isLoading: false
+    };
+  },
+  methods: {
+    async login() {
+      if (!!this.form.identifier && !!this.form.password) {
+        this.isLoading = true;
+        const res = await this.$store.dispatch("auth/login", this.form);
+        this.isLoading = false;
+        this.errorMsg = res;
+        if (res === true) {
+          this.$_options = false;
+          this.isLoading = false;
+          this.errorMsg = "";
+        }
+      }
+    }
+  },
+  computed: {
+    $_options: {
+      get: function() {
+        return this.dialogState;
+      },
+      set: function(val) {
+        this.$emit("update", val);
+        this.form.identifier = "";
+        this.form.password = "";
+      }
+    },
+    loginMessages() {
+      return this.$store.state.auth.loadingMessages;
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,7 +17,7 @@
         </q-toolbar-title>
         <div>
           <q-btn
-            @click="loginModal = true"
+            @click="loginDialog = true"
             color="yellow"
             unelevated
             class="mr-0 radius-6"
@@ -28,57 +28,7 @@
         </div>
       </q-toolbar>
     </q-header>
-
-    <q-dialog v-model="loginModal">
-      <q-card
-        class="radius-10 q-px-lg q-pt-lg q-pb-md"
-        style="width: 570px; max-width: 80vw;"
-      >
-        <q-card-section>
-          <q-form ref="loginForm" @submit.prevent="login" class="q-gutter-lg">
-            <div class="row">
-              <div class="col-12">
-                <p class="q-mb-sm font-16">Email</p>
-                <q-input
-                  outlined
-                  dense
-                  class="no-shadow input-radius-6"
-                  v-model="form.identifier"
-                  :rules="[val => !!val || 'Field is required']"
-                />
-              </div>
-              <div class="col-12 q-mt-lg">
-                <p class="q-mb-sm font-16">Password</p>
-                <q-input
-                  outlined
-                  dense
-                  type="password"
-                  class="no-shadow input-radius-6"
-                  v-model="form.password"
-                  :rules="[val => !!val || 'Field is required']"
-                />
-              </div>
-            </div>
-            <p class="no-margin text-red text-center">{{ errorMsg }}</p>
-            <div class="row justify-center">
-              <div class="col-6 text-center">
-                <q-btn
-                  type="submit"
-                  label="Login"
-                  class="radius-10"
-                  padding="12px 60px"
-                  color="yellow"
-                  text-color="black"
-                  :loading="isLoading"
-                  unelevated
-                />
-              </div>
-            </div>
-          </q-form>
-        </q-card-section>
-      </q-card>
-    </q-dialog>
-
+    <LoginDialog :dialogState="loginDialog" @update="loginDialog = $event" />
     <q-page-container>
       <router-view />
       <div v-if="$router.currentRoute.path != '/'" class="bg-yellow-10 q-py-xl">
@@ -106,37 +56,17 @@
 
 <script>
 import { date } from "quasar";
+import LoginDialog from "components/user/authentication/LoginDialog.vue";
 export default {
   name: "MainLayout",
+  components: {
+    LoginDialog
+  },
   data() {
     return {
-      form: {
-        identifier: "",
-        password: ""
-      },
-      errorMsg: "",
       currentYear: date.formatDate(Date.now(), "YYYY"),
-      loginModal: false,
-      isLoading: false
+      loginDialog: false
     };
-  },
-  methods: {
-    // May need to rework this....
-    async login() {
-      if (!!this.form.identifier && !!this.form.password) {
-        this.isLoading = true;
-        const res = await this.$store.dispatch("auth/login", this.form);
-        this.isLoading = false;
-        this.errorMsg = res;
-        if (res === true) {
-          this.loginModal = false;
-          this.isLoading = false;
-          this.form.identifier = "";
-          this.form.password = "";
-          this.errorMsg = "";
-        }
-      }
-    }
   }
 };
 </script>
