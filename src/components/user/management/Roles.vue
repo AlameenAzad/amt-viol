56c78ced1fd42689e8a0b1ea47852c9dff55faf3
bilendm <bilendmohammed@gmail.com>
2022-04-19 03:47:11 +0300

    Removed static data + Imported invite user component + small fixes

--- src/components/user/management/Roles.vue
+++ src/components/user/management/Roles.vue
@@ -5,8 +5,12 @@
       title="Current funding information"
       :data="data"
       :columns="columns"
+      :filter="filter"
       row-key="name"
-      hide-bottom
+      :hide-bottom="data.length > 0"
+      :pagination="{
+        rowsPerPage: 0
+      }"
     >
       <template v-slot:top>
         <div class="row full-width justify-between ">
@@ -32,7 +36,7 @@
               unelevated
               class="no-shadow radius-6 text-weight-600 "
               no-caps
-              @click="showDialog = true"
+              @click="inviteUserDialog = true"
             >
               <p class="q-mb-none q-mx-md q-my-sm">Invite User</p>
             </q-btn>
@@ -71,16 +75,17 @@
                     <q-item-section
                       ><span class="text-right font-14">
                         Edit
-                        <q-icon size="sm" class="text-blue" name="edit"/></span
-                    ></q-item-section>
+                        <q-icon size="sm" class="text-blue" name="edit" />
+                      </span>
+                    </q-item-section>
                   </q-item>
-
                   <q-item clickable v-close-popup>
                     <q-item-section
                       ><span class="text-right font-14 text-red">
                         Delete
-                        <q-icon size="sm" name="delete"/></span
-                    ></q-item-section>
+                        <q-icon size="sm" name="delete" />
+                      </span>
+                    </q-item-section>
                   </q-item>
                 </q-list>
               </q-menu>
@@ -89,241 +94,80 @@
         </q-tr>
       </template>
     </q-table>
-    <q-dialog v-model="showDialog">
-      <q-card class="q-pa-lg radius-10">
-        <div>
-          <h6 class="text-center font-24 q-mt-md">Invite User</h6>
-          <q-form ref="personalDataForm" class="q-gutter-lg q-px-md q-mb-md ">
-            <div class="row items-center ">
-              <div class="col-3">
-                <p class="font-14 no-margin">
-                  Email*
-                </p>
-              </div>
-              <div class="col-9">
-                <q-input
-                  outlined
-                  class="no-shadow input-radius-6"
-                  v-model="email"
-                  :rules="[]"
-                  label="E-Mail"
-                />
-              </div>
-            </div>
-            <div class="row items-center ">
-              <div class="col-3 ">
-                <p class="font-14 no-margin ">
-                  Role*
-                </p>
-              </div>
-              <div class="col-9">
-                <q-select
-                  outlined
-                  :options="options"
-                  class="no-shadow input-radius-6"
-                  v-model="role"
-                  :rules="[]"
-                  label="Editor"
-                />
-              </div>
-            </div>
-            <div class="row items-center">
-              <div class="col-3">
-                <p class="font-14 no-margin">
-                  Administration
-                </p>
-              </div>
-              <div class="col-9">
-                <q-select
-                  outlined
-                  :options="options"
-                  class="no-shadow input-radius-6"
-                  v-model="administration"
-                  :rules="[]"
-                  label="Select"
-                />
-              </div>
-            </div>
-            <div class="row items-baseline justify-evenly">
-              <div class="col-3">
-                <p class="font-14 no-margin">
-                  Message
-                </p>
-              </div>
-              <div class="col-9">
-                <q-input
-                  outlined
-                  class="no-shadow input-radius-6"
-                  v-model="message"
-                  :rules="[]"
-                  type="textarea"
-                  placeholder="text"
-                />
-              </div>
-            </div>
-
-            <div class="row justify-center q-ml-lg ">
-              <q-btn
-                label="Cancel"
-                outline
-                type="submit"
-                size="16px"
-                color="primary"
-                no-caps
-                class="no-shadow radius-6 q-px-xl  q-mr-sm "
-              />
-              <q-btn
-                label="Send Invitation"
-                type="submit"
-                unelevated
-                size="16px"
-                color="primary"
-                no-caps
-                class="  no-shadow	 radius-6 q-px-xl   "
-              />
-            </div>
-          </q-form>
-        </div>
-      </q-card>
-    </q-dialog>
+    <InviteUser
+      :dialogState="inviteUserDialog"
+      @update="inviteUserDialog = $event"
+    />
   </div>
 </template>
 
 <script>
+import InviteUser from "components/user/management/InviteUser.vue";
 export default {
   name: "Roles",
+  components: {
+    InviteUser
+  },
   data() {
     return {
-      options: ["Google", "Facebook", "Twitter", "Apple", "Oracle"],
-      showDialog: false,
-      role: "",
+      inviteUserDialog: false,
       filter: "",
-      administration: "",
-      email: "",
-      message: "",
       columns: [
         {
           name: "name",
           required: true,
           label: "Name",
           align: "left",
-          field: row => row.name,
+          field: row => row.username,
           format: val => `${val}`,
           sortable: true
         },
         {
-          name: "calories",
-          align: "center",
+          name: "email",
+          required: true,
           label: "Email",
-          field: "calories",
+          align: "left",
+          field: row => row.email,
+          format: val => `${val}`,
           sortable: true
         },
-        { name: "fat", label: "Adminstration", field: "fat", sortable: true },
-        { name: "carbs", label: "Role", field: "carbs" }
-      ],
-      data: [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: "14%",
-          iron: "1%"
+          name: "administration",
+          required: true,
+          label: "Administration",
+          align: "left",
+          field: row =>
+            (row["user_detail"] &&
+              row["user_detail"].municipality &&
+              row["user_detail"].municipality.title) ||
+            "Administration Unavailable",
+          format: val => `${val}`,
+          sortable: true
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: "8%",
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: "6%",
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: "3%",
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: "7%",
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: "0%",
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: "0%",
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: "0%",
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: "2%",
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: "12%",
-          iron: "6%"
+          name: "role",
+          required: true,
+          label: "role",
+          align: "left",
+          field: row => row.role.type || "Role Unavailable",
+          format: val => `${val}`,
+          sortable: true
         }
       ]
     };
+  },
+  methods: {
+    getData() {
+      this.$store.dispatch("userCenter/getUsers");
+    }
+  },
+  computed: {
+    data() {
+      return this.$store.state.userCenter.users;
+    }
+  },
+  mounted() {
+    this.getData();
   }
 };
 </script>
