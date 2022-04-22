b40a5f01bd245999aebe7b7b04fbbd74aa64295d
bilendm <bilendmohammed@gmail.com>
2022-04-22 03:12:47 +0300

    Removed static data + added function + added spinner

--- src/components/data/overview.vue
+++ src/components/data/overview.vue
@@ -141,14 +141,22 @@
             <q-btn size="md" color="primary" round flat dense icon="more_vert">
               <q-menu transition-show="jump-down" transition-hide="jump-up">
                 <q-list style="min-width: 140px">
-                  <q-item clickable v-close-popup>
+                  <q-item clickable @click="viewProject(props.row)">
                     <q-item-section
                       ><span class="text-right font-14">
                         View
                         <q-icon
+                          v-if="!isLoading"
                           size="sm"
                           class="text-blue"
-                          name="visibility"/></span
+                          name="visibility"
+                        />
+                        <q-spinner
+                          v-else
+                          color="primary"
+                          size="sm"
+                          :thickness="2"
+                        /> </span
                     ></q-item-section>
                   </q-item>
                   <q-item clickable v-close-popup>
@@ -281,112 +289,19 @@ export default {
           sortable: true
         },
         { name: "fat", label: "Status", field: "fat", sortable: true }
-      ]
-      // data: [
-      //   {
-      //     name: "Frozen Yogurt",
-      //     calories: 159,
-      //     fat: 6.0,
-      //     carbs: 24,
-      //     protein: 4.0,
-      //     sodium: 87,
-      //     calcium: "14%",
-      //     iron: "1%"
-      //   },
-      //   {
-      //     name: "Ice cream sandwich",
-      //     calories: 237,
-      //     fat: 9.0,
-      //     carbs: 37,
-      //     protein: 4.3,
-      //     sodium: 129,
-      //     calcium: "8%",
-      //     iron: "1%"
-      //   },
-      //   {
-      //     name: "Eclair",
-      //     calories: 262,
-      //     fat: 16.0,
-      //     carbs: 23,
-      //     protein: 6.0,
-      //     sodium: 337,
-      //     calcium: "6%",
-      //     iron: "7%"
-      //   },
-      //   {
-      //     name: "Cupcake",
-      //     calories: 305,
-      //     fat: 3.7,
-      //     carbs: 67,
-      //     protein: 4.3,
-      //     sodium: 413,
-      //     calcium: "3%",
-      //     iron: "8%"
-      //   },
-      //   {
-      //     name: "Gingerbread",
-      //     calories: 356,
-      //     fat: 16.0,
-      //     carbs: 49,
-      //     protein: 3.9,
-      //     sodium: 327,
-      //     calcium: "7%",
-      //     iron: "16%"
-      //   },
-      //   {
-      //     name: "Jelly bean",
-      //     calories: 375,
-      //     fat: 0.0,
-      //     carbs: 94,
-      //     protein: 0.0,
-      //     sodium: 50,
-      //     calcium: "0%",
-      //     iron: "0%"
-      //   },
-      //   {
-      //     name: "Lollipop",
-      //     calories: 392,
-      //     fat: 0.2,
-      //     carbs: 98,
-      //     protein: 0,
-      //     sodium: 38,
-      //     calcium: "0%",
-      //     iron: "2%"
-      //   },
-      //   {
-      //     name: "Honeycomb",
-      //     calories: 408,
-      //     fat: 3.2,
-      //     carbs: 87,
-      //     protein: 6.5,
-      //     sodium: 562,
-      //     calcium: "0%",
-      //     iron: "45%"
-      //   },
-      //   {
-      //     name: "Donut",
-      //     calories: 452,
-      //     fat: 25.0,
-      //     carbs: 51,
-      //     protein: 4.9,
-      //     sodium: 326,
-      //     calcium: "2%",
-      //     iron: "22%"
-      //   },
-      //   {
-      //     name: "KitKat",
-      //     calories: 518,
-      //     fat: 26.0,
-      //     carbs: 65,
-      //     protein: 7,
-      //     sodium: 54,
-      //     calcium: "12%",
-      //     iron: "6%"
-      //   }
-      // ]
+      ],
+      isLoading: false
     };
   },
   methods: {
+    async viewProject(row) {
+      this.isLoading = true;
+      const id = row && row.id;
+      await this.$store.dispatch("project/getSpecificProject", {
+        id: id
+      });
+      this.isLoading = false;
+    },
     goToPage(page) {
       if (page === "projectIdeas") {
         this.$router.push({ path: "/user/newProjectIdea" });
