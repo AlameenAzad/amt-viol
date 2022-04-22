07142060408c183b48bd9168c91529939ca48a39
bilendm <bilendmohammed@gmail.com>
2022-04-22 03:13:43 +0300

    Added project view page

---
+++ src/pages/dashboard/ProjectView.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class=" q-mx-xl q-mt-lg bg-blue-1">
+    <ProjectContent />
+  </q-page>
+</template>
+
+<script>
+import ProjectContent from "components/projects/view/ProjectContent.vue";
+export default {
+  name: "projectView",
+  components: {
+    ProjectContent
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.carouselThumbnails .q-tabs__arrow--end {
+  right: -70px;
+  color: $blue;
+}
+.carouselThumbnails .q-tabs__arrow--start {
+  left: -70px;
+  color: $blue;
+}
+</style>
