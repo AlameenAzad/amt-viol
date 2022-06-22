1d2609e6fb27309c28f9417ef96b2967a81df711
bilendm <bilendmohammed@gmail.com>
2022-06-22 20:07:26 +0300

    Added API data to table

--- src/components/funding/info.vue
+++ src/components/funding/info.vue
@@ -6,7 +6,7 @@
       :data="data"
       :columns="columns"
       row-key="name"
-      hide-bottom
+      :hide-bottom="data.length > 0"
       :pagination="{
         rowsPerPage: 0
       }"
@@ -14,7 +14,10 @@
       <template v-slot:top>
         <p class="font-24">{{ $t("fundingsInfo.current") }}</p>
         <q-space />
-        <p class="font-16 text-blue text-underline text-weight-600">
+        <p
+          @click="$router.push({ path: '/user/data?tab=fundings' })"
+          class="font-16 text-blue text-underline text-weight-600 cursor-pointer"
+        >
           {{ $t("fundingsInfo.showAll") }}
         </p>
       </template>
@@ -108,139 +111,59 @@
 export default {
   name: "fundingInfo",
   data() {
-    return {
-      data: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: "14%",
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: "8%",
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: "6%",
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: "3%",
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: "7%",
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: "0%",
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: "0%",
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: "0%",
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: "2%",
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: "12%",
-          iron: "6%"
-        }
-      ]
-    };
+    return {};
   },
   computed: {
+    data() {
+      return (
+        !!this.$store.state.funding.fundings &&
+        this.$store.state.funding.fundings
+      );
+    },
     columns() {
       return [
         {
-          name: "name",
+          name: "fundingInfo",
           required: true,
           label: this.$t("fundingsColsHome.fundInfo"),
           align: "left",
-          field: row => row.name,
-          format: val => `${val}`,
+          field: row => row.title,
           sortable: true
         },
         {
-          name: "calories",
-          align: "center",
+          name: "title",
+          align: "left",
           label: this.$t("fundingsColsHome.title"),
-          field: "calories",
+          field: row => row.title,
           sortable: true
         },
         {
-          name: "fat",
+          name: "categories",
+          align: "left",
           label: this.$t("fundingsColsHome.categories"),
-          field: "fat",
+          field: row =>
+            (!!row.categories &&
+              row.categories.map(category => category.title).join(", ")) ||
+            "No Categories",
           sortable: true
         },
-        { name: "carbs", label: "Author", field: "carbs" }
+        {
+          name: "owner",
+          align: "left",
+          label: "Owner",
+          field: row => (!!row.owner && row.owner.username) || "No Author",
+          sortable: true
+        }
       ];
     }
+  },
+  methods: {
+    getData() {
+      this.$store.dispatch("funding/getFundings");
+    }
+  },
+  mounted() {
+    this.getData();
   }
 };
 </script>
