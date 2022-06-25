4fa82077a2c3fdb312dc4c0f1d7de8f49f725104
bilendm <bilendmohammed@gmail.com>
2022-06-25 12:29:28 +0300

    Work on create method

--- src/store/implementationChecklist/actions.js
+++ src/store/implementationChecklist/actions.js
@@ -12,3 +12,45 @@ export async function getChecklists(context) {
     });
   }
 }
+
+export async function createNewChecklist(context, payload) {
+  const { data } = payload;
+  if (data) {
+    console.log("data", data);
+    // Construct initialContact
+    const initialContact = await constructInitialContact(data.initialContact);
+    console.log("initialContact", initialContact);
+  }
+}
+
+export async function constructInitialContact(payload) {
+  if (payload) {
+    let initialContact = {};
+    initialContact.start = payload.start;
+    initialContact.end = payload.end;
+    const captureIdea = payload.items.find(
+      item => item.objectName === "captureIdea"
+    );
+    const caputreContect = payload.items.find(
+      item => item.objectName === "caputreContect"
+    );
+    initialContact.captureIdea = {
+      id: captureIdea.id,
+      name: captureIdea.name,
+      text: captureIdea.text,
+      sortPosition: captureIdea.sortPosition,
+      active: captureIdea.active,
+      tasks: captureIdea.tasks,
+      project: !!captureIdea.project?.id ? { id: captureIdea.project.id } : {}
+    };
+    initialContact.caputreContect = {
+      id: caputreContect.id,
+      name: caputreContect.name,
+      text: caputreContect.text,
+      sortPosition: caputreContect.sortPosition,
+      active: caputreContect.active,
+      tasks: caputreContect.tasks
+    };
+    return initialContact;
+  }
+}
