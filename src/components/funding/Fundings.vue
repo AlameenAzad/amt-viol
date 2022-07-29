5cd0fb731d1a8920cf87d5562beea67a1ec7cef7
bilendm <bilendmohammed@gmail.com>
2022-07-29 13:54:39 +0300

    Added dynamic validation

--- src/components/funding/Fundings.vue
+++ src/components/funding/Fundings.vue
@@ -1,41 +1,52 @@
 <template>
-  <q-select
-    outlined
-    dense
-    v-model="model"
-    multiple
-    :options="fundings"
-    options-selected-class="text-primary text-weight-600"
-    class="no-shadow input-radius-6"
-    @input="onSelect"
-  >
-    <template v-slot:selected>
-      <template v-if="model && model.length > 0">
-        <span v-for="(funding, index) in model" :key="index">
-          {{ index > 0 ? ", " : "" }}
-          {{ funding.title }}
-        </span>
+  <div>
+    <q-select
+      outlined
+      dense
+      v-model="model"
+      multiple
+      :options="fundings"
+      options-selected-class="text-primary text-weight-600"
+      class="no-shadow input-radius-6"
+      @input="onSelect"
+      :rules="
+        requiresValidation === true
+          ? [val => (!!val && val.length > 0) || 'Required']
+          : []
+      "
+    >
+      <template v-slot:selected>
+        <template v-if="model && model.length > 0">
+          <span v-for="(funding, index) in model" :key="index">
+            {{ index > 0 ? ", " : "" }}
+            {{ funding.title }}
+          </span>
+        </template>
+        <template v-else>
+          <span class="text-grey">
+            {{ $t("fundingSelector.title") }}
+          </span>
+        </template>
       </template>
-      <template v-else>
-        <span class="text-grey">
-          {{ $t("fundingSelector.title") }}
-        </span>
+      <template v-slot:option="scope">
+        <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+          <q-item-section>
+            <q-item-label>{{ scope.opt.title }}</q-item-label>
+          </q-item-section>
+        </q-item>
       </template>
-    </template>
-    <template v-slot:option="scope">
-      <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
-        <q-item-section>
-          <q-item-label>{{ scope.opt.title }}</q-item-label>
-        </q-item-section>
-      </q-item>
-    </template>
-  </q-select>
+    </q-select>
+  </div>
 </template>
 
 <script>
 export default {
   name: "fundings",
   props: {
+    requiresValidation: {
+      type: Boolean,
+      default: false
+    },
     editing: {
       type: Array,
       default: () => []
