3e35236a1ac14986a57d31b5079d43adb2c71d9e
AlameenAzad <alameen.azad@protonmail.com>
2022-07-30 16:17:46 +0300

    add notifications call to action functions

--- src/pages/dashboard/Notifications.vue
+++ src/pages/dashboard/Notifications.vue
@@ -1,8 +1,9 @@
 <template>
   <q-page class="q-mx-xl q-mt-lg q-pb-md">
     <div v-for="(cluster, index) in data" :key="index">
-      <p class="font-16">{{ index }}</p>
-      <q-card v-for="noti in cluster" :key="noti.id+noti.typeOfNoti" class="shadow-1 radius-20 q-mb-md q-pa-sm" flat>
+      <p class="font-16">{{ getIndex(index) }}</p>
+      <q-card v-for="(noti, index) in cluster" :key="noti.id+noti.typeOfNoti" class="shadow-1 radius-20 q-mb-md q-pa-sm"
+        flat>
         <q-card-section class="row">
           <q-icon :name="getIcon(noti.typeOfNoti)" size="md" color="blue-5" class="q-mr-sm" />
           <div class="col">
@@ -14,32 +15,39 @@
               made by | `:"" }}
 
               <span class="text-blue">{{ noti.typeOfNoti == "fundingExpirey" ? noti.plannedEnd :
-              noti.typeOfNoti == "fundingComments" ? noti.owner.username : noti.typeOfNoti == 'requests'?
-              noti.user.username : "" }}</span>
+                noti.typeOfNoti == "fundingComments" ? noti.owner.username : noti.typeOfNoti == 'requests'?
+                noti.user.username : "" }}</span>
 
             </p>
             <p class="font-14 q-mb-none">{{ noti.typeOfNoti == "fundingExpirey" ? noti.title : noti.typeOfNoti ==
-            "guest" ?
-            noti.email : noti.typeOfNoti == "fundingComments" ? noti.funding.title : noti.typeOfNoti == 'requests' ?
-            noti.project != null ? noti.project.title : noti.funding != null ? noti.funding.title :
-            noti.checklist.title :"" }}
+              "guest" ?
+              noti.email : noti.typeOfNoti == "fundingComments" ? noti.funding.title : noti.typeOfNoti == 'requests' ?
+              noti.project != null ? noti.project.title : noti.funding != null ? noti.funding.title :
+              noti.checklist.title :"" }}
             </p>
           </div>
           <div class="text-right">
             <q-btn @click="view(noti)"
-              v-if="noti.typeOfNoti == 'fundingComments' || noti.typeOfNoti == 'fundingExpirey'" color="blue" unelevated
-              outline class="radius-6 text-weight-600" no-caps>
+              v-if="noti.typeOfNoti == 'fundingComments' || noti.typeOfNoti == 'fundingExpirey' || noti.typeOfNoti == 'requests'"
+              color="
+              blue" unelevated outline class="radius-6 text-weight-600" no-caps>
               <p class="q-mb-none q-mx-xl q-my-sm">
                 {{ $t("notificationsUser.viewBtn") }}
               </p>
             </q-btn>
-            <q-btn v-if="noti.typeOfNoti == 'guest' || noti.typeOfNoti == 'requests' || noti.typeOfNoti == 'requests'"
+            <q-btn @click="view(noti, true)" v-if="noti.typeOfNoti == 'fundingComments'" color="
+              blue" unelevated outline class="radius-6 text-weight-600 q-ml-md" no-caps>
+              <p class="q-mb-none q-mx-xl q-my-sm">
+                {{ $t("notificationsUser.viewFundingBtn") }}
+              </p>
+            </q-btn>
+            <q-btn @click="acceptReq(noti, index)" v-if="noti.typeOfNoti == 'guest' || noti.typeOfNoti == 'requests'"
               color="blue" unelevated class="radius-6 q-ml-md text-weight-600" no-caps>
               <p class="q-mb-none q-mx-xl q-my-sm">
                 {{ $t("notificationsUser.acceptBtn") }}
               </p>
             </q-btn>
-            <q-btn v-if="noti.typeOfNoti == 'guest' || noti.typeOfNoti == 'requests' || noti.typeOfNoti == 'requests'"
+            <q-btn @click="declineReq(noti, index)" v-if="noti.typeOfNoti == 'guest' || noti.typeOfNoti == 'requests'"
               color="red" unelevated class="radius-6 q-ml-md text-weight-600" no-caps>
               <p class="q-mb-none q-mx-xl q-my-sm">
                 {{ $t("notificationsUser.declineBtn") }}
@@ -56,7 +64,7 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          {{ currentFundingComment.owner.username }} says: <br/> {{ currentFundingComment.comment }}
+          {{ currentFundingComment.owner.username }} says: <br /> {{ currentFundingComment.comment }}
         </q-card-section>
 
         <q-card-actions align="right">
@@ -64,20 +72,34 @@
         </q-card-actions>
       </q-card>
     </q-dialog>
+    <InviteUser :dialogState="inviteUserDialog" @update="inviteUserDialog = $event" :guestEmail="currentGuestEmail" />
   </q-page>
 </template>
 
 <script>
+import { date } from 'quasar';
+import InviteUser from "components/user/management/InviteUser.vue";
 export default {
   name: "notifications",
+  components: {
+    InviteUser
+  },
   data() {
     return {
       data: [],
       dialog: false,
-      currentFundingComment: null
+      currentFundingComment: null,
+      inviteUserDialog: false,
+      currentGuestEmail:"",
     };
   },
   methods: {
+    getIndex(index){
+      if (date.formatDate(new Date(), 'YYYY-MM-DD') == index)
+        return "Today";
+      else
+        return index;
+    },
     prepData(){
       var data = [];
       for (const item in this.data) {
@@ -123,17 +145,54 @@ export default {
       else if (type == "guest") return "person";
       else if (type == "requests") return "person_add";
     },
-    async view(noti){
-      if (noti.typeOfNoti == "fundingComments"){
+    async view(noti, isFunding = false){
+      if (noti.typeOfNoti == "fundingComments" && isFunding){
+        this.$router.push({ path: `/user/newFunding/${noti.id}` });
+      }
+      else if (noti.typeOfNoti == "fundingComments"){
         this.dialog = true;
         this.currentFundingComment = noti;
       }
-      if (noti.typeOfNoti == "fundingExpirey"){
-        await this.$store.dispatch("funding/getSpecificFunding", {
-          id: noti.id
-        });
+      else if (noti.typeOfNoti == "fundingExpirey"){
+        // await this.$store.dispatch("funding/getSpecificFunding", {
+        //   id: noti.id
+        // });
         this.$router.push({ path: `/user/newFunding/${noti.id}` });
+      } else if (noti.typeOfNoti == 'requests'){
+        if(noti.project != null){
+          this.$router.push({ path: `/user/newProjectIdea/${noti.project.id}` });
+        } else if(noti.funding != null){
+          this.$router.push({ path: `/user/newFunding/${noti.funding.id}` });
+        } else if(noti.checklist != null){
+          this.$router.push({ path: `/user/newChecklist/${noti.checklist.id}` });
+        }
       }
+    },
+    acceptReq(noti, index){
+      if (noti.typeOfNoti == 'requests'){
+      this.$store.dispatch("userCenter/manageRequest", {
+        id: noti.id, val : true
+      });
+      this.updateNotifications(noti, index);
+      }else{
+        this.currentGuestEmail = noti.email;
+        this.inviteUserDialog = true;
+      }
+    },
+    declineReq(noti, index){
+      if (noti.typeOfNoti == 'requests')
+      this.$store.dispatch("userCenter/manageRequest", {
+        id: noti.id, val : false
+      });
+      else{
+        this.$store.dispatch("userCenter/deleteGuestRequest", {
+        id: noti.id
+      });
+      }
+      this.updateNotifications(noti, index);
+    },
+    updateNotifications(noti, index){
+      this.data[noti.createdAt.split("T")[0]].splice(index,1);
     }
   },
   mounted() {
