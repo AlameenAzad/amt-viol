3157cf6f6bcd7cb61618613807f1c92dab2ad28e
bilendm <bilendmohammed@gmail.com>
2022-07-30 18:09:06 +0300

    Removed unused function

--- src/layouts/dashboardLayout.vue
+++ src/layouts/dashboardLayout.vue
@@ -165,10 +165,6 @@ export default {
           sepia: 10
         });
     },
-    goBack() {
-      this.$router.currentRoute.meta.backLink &&
-        this.$router.push(this.$router.currentRoute.meta.backLink);
-    },
     logout() {
       console.log(localStorage.getItem("showLogoutDialog"));
       localStorage.getItem("showLogoutDialog") == "false"
