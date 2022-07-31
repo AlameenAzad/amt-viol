147e11b8b023b3f7f569c426d736f9037dc1d039
bilendm <bilendmohammed@gmail.com>
2022-08-01 01:17:29 +0300

    Added buttons and functions

--- src/components/Municipality/municipalityDetail.vue
+++ src/components/Municipality/municipalityDetail.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="q-my-lg " :class="$q.screen.gt.sm ? 'q-mx-xl' : 'q-mx-sm'">
     <div class="row q-col-gutter-md">
-      <div class="col-12 col-md-auto">
-        <q-card class="left-card radius-20 flex flex-center">
-          <q-card-section>
-            <!-- TODO make this q-img -->
-            <img class="logo" al src="../../assets/Wappen.png" />
-          </q-card-section>
-        </q-card>
-      </div>
+      <!-- <div class="col-12 col-md-auto"> -->
+      <!-- <q-card class="left-card radius-20 flex flex-center"> -->
+      <!-- <q-card-section> -->
+      <!-- TODO make this q-img -->
+      <!-- <img class="logo" al src="../../assets/Wappen.png" /> -->
+      <!-- </q-card-section> -->
+      <!-- </q-card> -->
+      <!-- </div> -->
       <div class="col-12 col-md-grow">
         <q-card class="radius-20 bg-blue-1 right-card">
           <q-card-section>
@@ -33,7 +33,7 @@
                 <p
                   v-for="(user, index) in municipality.users.split(', ')"
                   :key="index"
-                  class="q-mb-none"
+                  class="q-mb-xs"
                 >
                   {{ user }}
                 </p>
@@ -56,7 +56,7 @@
             >
               <q-menu transition-show="jump-down" transition-hide="jump-up">
                 <q-list style="min-width: 140px">
-                  <q-item clickable v-close-popup>
+                  <q-item clickable v-close-popup @click="editMunicipality">
                     <q-item-section
                       ><span class="text-right font-14">
                         {{ $t("DetailsAdministration.edit") }}
@@ -64,7 +64,7 @@
                     ></q-item-section>
                   </q-item>
 
-                  <q-item clickable v-close-popup>
+                  <q-item clickable v-close-popup @click="deleteMunicipality">
                     <q-item-section
                       ><span class="text-right text-red font-14">
                         {{ $t("DetailsAdministration.delete") }}
@@ -132,7 +132,7 @@
                     ></q-item-section>
                   </q-item>
 
-                  <q-item clickable v-close-popup>
+                  <q-item clickable v-close-popup @click="editItem(props.row)">
                     <q-item-section
                       ><span class="text-right font-14">
                         {{ $t("DetailsAdministration.edit") }}
@@ -140,7 +140,11 @@
                         <q-icon size="sm" class="text-blue" name="edit"/></span
                     ></q-item-section>
                   </q-item>
-                  <q-item clickable v-close-popup>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    @click="addToWatchlist(props.row)"
+                  >
                     <q-item-section
                       ><span class="text-right font-14">
                         {{ $t("DetailsAdministration.bookmark") }}
@@ -148,7 +152,18 @@
                         <q-icon size="sm" class="text-blue" name="star"/></span
                     ></q-item-section>
                   </q-item>
-                  <q-item clickable v-close-popup>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    @click="archiveItem(props.row)"
+                    v-if="
+                      isAdmin ||
+                        (props.row &&
+                          props.row.type !== 'funding' &&
+                          (!!props.row.owner && props.row.owner.id) ===
+                            (!!loggedInUser && loggedInUser.id))
+                    "
+                  >
                     <q-item-section
                       ><span class="text-right font-14">
                         {{ $t("DetailsAdministration.archive") }}
@@ -159,7 +174,12 @@
                     ></q-item-section>
                   </q-item>
 
-                  <q-item clickable v-close-popup>
+                  <q-item
+                    v-if="isAdmin"
+                    clickable
+                    v-close-popup
+                    @click="deleteItem(props.row)"
+                  >
                     <q-item-section
                       ><span class="text-right font-14 text-red">
                         {{ $t("DetailsAdministration.delete") }}
@@ -173,16 +193,58 @@
         </q-tr>
       </template>
     </q-table>
+    <MunicipalityDeleteDialog
+      :dialogState="deleteMunicipalityDialog"
+      :id="municipalityId"
+      @update="(deleteMunicipalityDialog = $event), (municipalityId = null)"
+    />
+    <EditMunicipalityDialog
+      :dialogState="editMunicipalityDialog"
+      :editingId="municipalityId"
+      @update="(editMunicipalityDialog = $event), (municipalityId = null)"
+    />
+    <DeleteDialog
+      :id="itemId"
+      :tab="tab"
+      :dialogState="deleteDialog"
+      @update="closeDialog($event), (itemId = null), (tab = null)"
+    />
+    <RequestAccessDialog
+      :id="itemId"
+      :tab="tab"
+      :type="itemType"
+      :dialogState="requestDialog"
+      @update="(requestDialog = $event), (itemId = null), (itemType = null)"
+    />
   </div>
 </template>
 
 <script>
+import MunicipalityDeleteDialog from "components/Municipality/DeleteDialog.vue";
+import EditMunicipalityDialog from "components/Municipality/CreateDialog.vue";
+import DeleteDialog from "components/data/DeleteDialog.vue";
+import RequestAccessDialog from "components/data/RequestAccessDialog.vue";
+
 export default {
   data() {
     return {
       visibleColumns: ["title", "type", "categories", "users"],
+      deleteMunicipalityDialog: false,
+      editMunicipalityDialog: false,
+      deleteDialog: false,
+      requestDialog: false,
+      municipalityId: null,
+      itemId: null,
+      tab: null,
+      itemType: null,
       municipality: {}
     };
+  },
+  components: {
+    MunicipalityDeleteDialog,
+    EditMunicipalityDialog,
+    DeleteDialog,
+    RequestAccessDialog
   },
   methods: {
     getMunicipalityData() {
@@ -209,8 +271,241 @@ export default {
         this.$router.push("/Administation/Areas");
       }
     },
-    view(row) {
+    async view(row) {
       console.log("row", row);
+      const id = row && row.id;
+      this.itemId = row && row.id;
+      this.viewIsLoading = true;
+      if (row.type === "project") {
+        this.tab = "projectIdeas";
+        if (
+          row.visibility === "listed only" &&
+          (!!row.owner && row.owner.id) !==
+            (!!this.loggedInUser && this.loggedInUser.id) &&
+          !this.isAdmin
+        ) {
+          const hasReaderAccess =
+            !!row.readers &&
+            row.readers.map(
+              user => user.id === (!!this.loggedInUser && this.loggedInUser.id)
+            );
+          if (hasReaderAccess.length > 0) {
+            this.$router.push({ path: `/user/newProjectIdea/${id}` });
+          } else {
+            this.itemId = row && row.id;
+            this.itemType = "view";
+            this.requestDialog = true;
+          }
+        } else {
+          this.$router.push({ path: `/user/newProjectIdea/${id}` });
+        }
+      } else if (row.type === "funding") {
+        this.tab = "fundings";
+        if (
+          row.visibility === "listed only" &&
+          (!!row.owner && row.owner.id) !==
+            (!!this.loggedInUser && this.loggedInUser.id) &&
+          !this.isAdmin
+        ) {
+          const hasReaderAccess =
+            !!row.readers &&
+            row.readers.map(
+              user => user.id === (!!this.loggedInUser && this.loggedInUser.id)
+            );
+          if (hasReaderAccess.length > 0) {
+            this.$router.push({ path: `/user/newFunding/${id}` });
+          } else {
+            this.itemId = row && row.id;
+            this.itemType = "view";
+            this.requestDialog = true;
+          }
+        } else {
+          this.$router.push({ path: `/user/newFunding/${id}` });
+        }
+      } else {
+        console.log("checklist");
+        this.tab = "implementationChecklist";
+        if (
+          row.visibility === "listed only" &&
+          (!!row.owner && row.owner.id) !==
+            (!!this.loggedInUser && this.loggedInUser.id) &&
+          !this.isAdmin
+        ) {
+          const hasReaderAccess =
+            !!row.readers &&
+            row.readers.map(
+              user => user.id === (!!this.loggedInUser && this.loggedInUser.id)
+            );
+          if (hasReaderAccess.length > 0) {
+            this.$router.push({ path: `/user/newChecklist/${id}` });
+          } else {
+            this.itemId = row && row.id;
+            this.itemType = "view";
+            this.requestDialog = true;
+          }
+        } else {
+          this.$router.push({ path: `/user/newChecklist/${id}` });
+        }
+      }
+      this.viewIsLoading = false;
+    },
+    async editItem(row) {
+      const id = row && row.id;
+      this.itemId = row && row.id;
+      this.editIsLoading = true;
+      if (row.type === "project") {
+        this.tab = "projectIdeas";
+        if (
+          row.visibility === "listed only" &&
+          (!!row.owner && row.owner.id) !==
+            (!!this.loggedInUser && this.loggedInUser.id) &&
+          !this.isAdmin
+        ) {
+          const hasEditorAccess =
+            !!row.editors &&
+            row.editors.map(
+              user => user.id === (!!this.loggedInUser && this.loggedInUser.id)
+            );
+          if (hasEditorAccess.length > 0) {
+            this.$router.push({ path: `/user/newProjectIdea/edit/${id}` });
+          } else {
+            this.itemId = row && row.id;
+            this.itemType = "edit";
+            this.requestDialog = true;
+          }
+        } else {
+          this.$router.push({ path: `/user/newProjectIdea/edit/${id}` });
+        }
+      } else if (row.type === "funding") {
+        this.tab = "fundings";
+        if (
+          row.visibility === "listed only" &&
+          (!!row.owner && row.owner.id) !==
+            (!!this.loggedInUser && this.loggedInUser.id) &&
+          !this.isAdmin
+        ) {
+          const hasEditorAccess =
+            !!row.editors &&
+            row.editors.map(
+              user => user.id === (!!this.loggedInUser && this.loggedInUser.id)
+            );
+          if (hasEditorAccess.length > 0) {
+            this.$router.push({ path: `/user/newFunding/edit/${id}` });
+          } else {
+            this.itemId = row && row.id;
+            this.itemType = "edit";
+            this.requestDialog = true;
+          }
+        } else {
+          this.$router.push({ path: `/user/newFunding/edit/${id}` });
+        }
+      } else {
+        this.tab = "implementationChecklist";
+        if (
+          row.visibility === "listed only" &&
+          (!!row.owner && row.owner.id) !==
+            (!!this.loggedInUser && this.loggedInUser.id) &&
+          !this.isAdmin
+        ) {
+          const hasEditorAccess =
+            !!row.editors &&
+            row.editors.map(
+              user => user.id === (!!this.loggedInUser && this.loggedInUser.id)
+            );
+          if (hasEditorAccess.length > 0) {
+            this.$router.push({ path: `/user/newChecklist/edit/${id}` });
+          } else {
+            this.itemId = row && row.id;
+            this.itemType = "edit";
+            this.requestDialog = true;
+          }
+        } else {
+          this.$router.push({ path: `/user/newChecklist/edit/${id}` });
+        }
+      }
+      this.editIsLoading = false;
+    },
+    async archiveItem(row) {
+      if (row.type === "project") {
+        this.archiveIsLoading = true;
+        const id = row && row.id;
+        await this.$store.dispatch("project/archiveProjectIdea", {
+          id: id
+        });
+        this.archiveIsLoading = false;
+        this.getData();
+      } else if (row.type === "funding") {
+        this.archiveIsLoading = true;
+        const id = row && row.id;
+        await this.$store.dispatch("funding/archiveFunding", {
+          id: id
+        });
+        this.archiveIsLoading = false;
+        this.getData();
+      } else {
+        this.archiveIsLoading = true;
+        const id = row && row.id;
+        await this.$store.dispatch("implementationChecklist/archiveChecklist", {
+          id: id
+        });
+        this.archiveIsLoading = false;
+        this.getData();
+      }
+    },
+    async addToWatchlist(row) {
+      if (row.type === "project") {
+        this.watchlistIsLoading = true;
+        const id = row && row.id;
+        await this.$store.dispatch("project/addToWatchlist", {
+          id: id
+        });
+        this.watchlistIsLoading = false;
+        this.getData();
+      } else if (row.type === "funding") {
+        this.watchlistIsLoading = true;
+        const id = row && row.id;
+        await this.$store.dispatch("funding/addToWatchlist", {
+          id: id
+        });
+        this.watchlistIsLoading = false;
+        this.getData();
+      } else {
+        this.watchlistIsLoading = true;
+        const id = row && row.id;
+        await this.$store.dispatch("implementationChecklist/addToWatchlist", {
+          id: id
+        });
+        this.watchlistIsLoading = false;
+        this.getData();
+      }
+    },
+    async deleteItem(row) {
+      if (row.type === "project") {
+        this.tab = "projectIdeas";
+        this.itemId = row && row.id;
+      } else if (row.type === "funding") {
+        this.tab = "fundings";
+        this.itemId = row && row.id;
+      } else {
+        this.tab = "implementationChecklist";
+        this.itemId = row && row.id;
+      }
+      this.deleteDialog = true;
+    },
+    deleteMunicipality() {
+      this.municipalityId = !!this.municipality && this.municipality.id;
+      this.deleteMunicipalityDialog = true;
+    },
+    editMunicipality() {
+      this.municipalityId = !!this.municipality && this.municipality.id;
+      this.editMunicipalityDialog = true;
+    },
+    closeDialog(val) {
+      this.deleteDialog = val;
+      this.getData();
+    },
+    getData() {
+      this.$store.dispatch("municipality/getMunicipalities");
     }
   },
   mounted() {
