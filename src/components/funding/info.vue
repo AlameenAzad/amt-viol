0d323973115afd58e98694d1ea1f0fe20323bc66
bilendm <bilendmohammed@gmail.com>
2022-08-05 14:12:14 +0300

    Translated 'Records per page'

--- src/components/funding/info.vue
+++ src/components/funding/info.vue
@@ -11,8 +11,9 @@
         sortBy: 'id',
         descending: true,
         page: 1,
-        rowsPerPage: 50
+        rowsPerPage: 10
       }"
+      :rows-per-page-label="$t('Records per page')"
     >
       <template v-slot:top>
         <p class="font-24">{{ $t("fundingsInfo.current") }}</p>
