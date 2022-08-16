837aec165e402b1da1bc66f2aa31e349dca2e7f9
bilendm <bilendmohammed@gmail.com>
2022-08-16 15:15:51 +0300

    Moved cookie box

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -171,17 +171,18 @@
         <div class="col-12 col-md-8">
           <div class="row q-col-gutter-xl">
             <div class="col-12 col-md-4">
-              <q-card class="full-height full-width shadow-0">
-                <q-card-section class="full-height flex center">
+              <q-card class="full-height full-width  shadow-0">
+                <q-card-section class="full-height flex flex-center">
                   <q-img
                     spinner-color="primary"
-                    src="logo-1.svg"
+                    src="logo-3.svg"
                     height="150px"
                     contain
                   />
                 </q-card-section>
               </q-card>
             </div>
+
             <div class="col-12 col-md-4">
               <q-card class="full-height full-width  shadow-0">
                 <q-card-section class="full-height flex flex-center">
@@ -195,11 +196,11 @@
               </q-card>
             </div>
             <div class="col-12 col-md-4">
-              <q-card class="full-height full-width  shadow-0">
-                <q-card-section class="full-height flex flex-center">
+              <q-card class="full-height full-width shadow-0">
+                <q-card-section class="full-height flex center">
                   <q-img
                     spinner-color="primary"
-                    src="logo-3.svg"
+                    src="logo-1.svg"
                     height="150px"
                     contain
                   />
@@ -210,128 +211,6 @@
         </div>
       </div>
     </section>
-    <q-card class="fixed-bottom full-width q-mx-auto q-pa-lg shadow-2">
-      <div class="row justify-center">
-        <div class="col-12 col-md-10">
-          <div class="row items-center">
-            <div class="col-12 col-md-8">
-              <q-card-section class="q-pb-none">
-                <div class="row justify-between">
-                  <div class="col-6">
-                    <p class="no-margin font-24">This website uses Cookies</p>
-                  </div>
-                </div>
-              </q-card-section>
-              <q-card-section class="">
-                <div class="row">
-                  <div class="col-12">
-                    By clicking "Accept All Cookies", you are agree to the
-                    storing of cookies on your device to enhance site
-                    navigation, analyze site usage, and assist in our marketting
-                    efforts. See our Privacy Policy for more information
-                  </div>
-                </div>
-              </q-card-section>
-            </div>
-            <div class="col-12 col-md-4">
-              <q-card-section class="">
-                <div class="col-12">
-                  <q-btn
-                    padding="10px 0"
-                    unelevated
-                    color="primary"
-                    class="shadow-0 full-width"
-                  >
-                    Accept All Cookies
-                  </q-btn>
-                </div>
-                <div class="col-12 q-mt-sm">
-                  <q-btn
-                    padding="10px 0"
-                    outline
-                    color="primary"
-                    class="shadow-0 full-width"
-                  >
-                    Accept Essential
-                  </q-btn>
-                </div>
-              </q-card-section>
-            </div>
-            <div class="col-12">
-              <q-card-section>
-                <div class="row q-col-gutter-x-md justify-between items-center">
-                  <div class="col-auto">
-                    <div class="row">
-                      <div class="col-auto">
-                        <q-toggle
-                          size="xl"
-                          color="primary"
-                          v-model="essential"
-                          disable
-                          label="Essential"
-                          left-label
-                        />
-                      </div>
-                      <div class="col-auto">
-                        <q-toggle
-                          size="xl"
-                          color="primary"
-                          v-model="easeOfUse"
-                          label="User preferences"
-                          left-label
-                        />
-                      </div>
-                      <div class="col-auto">
-                        <q-toggle
-                          color="primary"
-                          size="xl"
-                          v-model="necessaryEmails"
-                          label="Neceessary E-Mails"
-                          left-label
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-auto">
-                    <div class="row">
-                      <div class="col-auto">
-                        <q-btn
-                          class="radius-6"
-                          unelevated
-                          color="primary"
-                          @click="expanded = !expanded"
-                          label="Details"
-                          :icon-right="expanded ? 'expand_less' : 'expand_more'"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-12">
-                    <q-expansion-item
-                      header-class="hidden"
-                      expand-icon="0"
-                      v-model="expanded"
-                    >
-                      <q-card>
-                        <q-card-section>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Quidem, eius reprehenderit eos corrupti commodi
-                          magni quaerat ex numquam, dolorum officiis modi facere
-                          maiores architecto suscipit iste eveniet doloribus
-                          ullam aliquid.
-                        </q-card-section>
-                      </q-card>
-                    </q-expansion-item>
-                  </div>
-                </div>
-              </q-card-section>
-            </div>
-          </div>
-        </div>
-      </div>
-    </q-card>
   </q-page>
 </template>
 
@@ -340,11 +219,7 @@ export default {
   name: "index",
   data() {
     return {
-      documentSearch: "",
-      essential: true,
-      easeOfUse: false,
-      necessaryEmails: false,
-      expanded: false
+      documentSearch: ""
     };
   },
   methods: {
