f687c656f1dd58ed93eec6e544fe987e17e88b72
bilendm <bilendmohammed@gmail.com>
2022-08-26 15:31:31 +0300

    Translated Search

--- src/components/network/overview.vue
+++ src/components/network/overview.vue
@@ -19,7 +19,7 @@
           outlined
           class="input-radius-6 no-shadow q-mb-sm q-mt-sm"
           v-model="filter"
-          placeholder="Search"
+          :placeholder="$t('Search')"
           dense
         >
           <template v-slot:prepend>
